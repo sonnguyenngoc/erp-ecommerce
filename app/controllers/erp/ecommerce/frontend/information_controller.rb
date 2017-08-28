4496e1a24d3b5d64dc029b3b6dd5b424637795d0
Son Nguyen <sonnn@hoangkhang.com.vn>
2017-08-28 09:31:43 +0700

    add source

---
+++ app/controllers/erp/ecommerce/frontend/information_controller.rb
@@ -0,0 +1,13 @@
+module Erp
+  module Ecommerce
+    module Frontend
+      class InformationController < Erp::Frontend::FrontendController
+        def contact
+        end
+        
+        def about_us
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Erp::Ecommerce::Engine.routes.draw do
   root to: "frontend/coming#index"
   get "trang-chu.html" => "frontend/home#index", as: :home
+  get "gioi-thieu.html" => "frontend/information#about_us", as: :about_us
+  get "lien-he.html" => "frontend/information#contact", as: :contact
 end
